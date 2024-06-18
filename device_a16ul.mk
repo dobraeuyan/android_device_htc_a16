682222e3142664847da325e55ecc5c01521dc5e9
fernan_sssr <87738348+dobraeuyan@users.noreply.github.com>
2024-06-18 16:21:48 -0600

    Update device_a16ul.mk

--- device_a16ul.mk
+++ device_a16ul.mk
@@ -1,25 +1,33 @@
+# Copyright (C) 2012 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # The gps config appropriate for this device
 $(call inherit-product, device/common/gps/gps_us_supl.mk)
 
-$(call inherit-product-if-exists, vendor/htc/a16ul/a16ul-vendor.mk)
+$(call inherit-product-if-exists, vendor/htc/a16/a16-vendor.mk)
 
-DEVICE_PACKAGE_OVERLAYS += device/htc/a16ul/overlay
+#DEVICE_PACKAGE_OVERLAYS += device/htc/a16/overlay
 
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+$(call inherit-product, device/htc/a16/device.mk)
 
-ifeq ($(TARGET_PREBUILT_KERNEL),)
-	LOCAL_KERNEL := device/htc/a16ul/kernel
-else
-	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
-endif
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_KERNEL):kernel \
-    $(LOCAL_PATH)/recovery/init.recovery.htc_a16.rc:root/init.recovery.htc_a16.rc
-
-$(call inherit-product, build/target/product/full.mk)
-
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
-PRODUCT_NAME := full_a16ul
-PRODUCT_DEVICE := a16ul
+# Discard inherited values and use our own instead.
+PRODUCT_NAME := lineage_a16
+PRODUCT_BRAND := htc
+PRODUCT_DEVICE := a16
+PRODUCT_MODEL := a16
+PRODUCT_MANUFACTURER := htc
